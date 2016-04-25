f013a12a81e0dcd627360def62cb0bf566c8783e
Jonathan Keane <jkeane@gmail.com>
2016-04-25 17:22:59 -0500

    Moved installation to a new file, changes process to be use the new makeElanFiles function

---
+++ inst/install.R
@@ -0,0 +1,8 @@
+# to install ElementTree:
+# pip install --user ElementTree
+# or #
+# download elementtree and then:
+# python setup.py install --user
+
+library(devtools)
+install_github("jonkeane/mocapGrip")
--- inst/process.R
+++ inst/process.R
@@ -1,16 +1,5 @@
-# to install ElementTree:
-# pip install --user ElementTree
-# or #
-# download elementtree and then:
-# python setup.py install --user
-
 cmdArgs <- commandArgs(trailingOnly = TRUE)
 
-# load_all("./mocapProcessor")
-
-# install.packages("./mocapProcessor", repos = NULL, type="source")
-library(devtools)
-install_github("jonkeane/mocapGrip")
 library(mocapGrip)
 
-mainFunc(cmdArgs) -> output
+makeElanFiles(cmdArgs) -> output
