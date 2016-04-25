712ec034b75ce1834519282112af3e69e9f59b06
Jonathan Keane <jkeane@gmail.com>
2016-04-25 01:36:29 -0500

    Checking Travis

--- tests/testthat/testTimeConversions.R
+++ tests/testthat/testTimeConversions.R
@@ -4,5 +4,6 @@ context("Time conversions")
 test_that("timeToSecs returns the right number", {
   expect_equal(mocapGrip:::timeToSecs("01:01:01:00"), 60*60+60+1+(1*(1/30)))
   expect_equal(mocapGrip:::timeToSecs("02:02:02:01"), 2*60*60+2*60+2+(2*(1/30)))
+  expect_equal(mocapGrip:::timeToSecs("03:03:03:02"), 3*60*60+3*60+3+(3*(1/30)))
   expect_equal(mocapGrip:::timeToSecs("01:01:01:15", fps = 60), 60*60+60+1+(16*(1/60)))
 })
