0c67f3d7ad7e53e3c15e3c000639310692d34116
Jonathan Keane <jkeane@gmail.com>
2016-04-26 16:36:58 -0500

    updated one path to check if travis can read it.

--- tests/testthat/testDistanceCalculations.R
+++ tests/testthat/testDistanceCalculations.R
@@ -1,14 +1,14 @@
 library(mocapGrip)
 context("distance calculationss")
 
-load(file.path("extractedMarkerData.Rdata")) # markerDataHead
-load(file.path("dist57.RData")) # dist57head
+load(file.path('.','tests','testthat','extractedMarkerData.Rdata')) # markerDataHead
+load(file.path('dist57.RData')) # dist57head
 
 test_that("calculateDistances returns the correct distances", {
   expect_equal(mocapGrip:::calculateDistances(markerDataHead, c(5,7)), dist57head)
 })
 
-load(file.path("meanData.Rdata")) # meanDataHead
+load(file.path('meanData.Rdata')) # meanDataHead
 
 test_that("meanOnAxis returns the correct distances", {
   expect_equal(mocapGrip:::meanOnAxis(markerDataHead, c(0, 1, 2, 3, 4), axis ="Y"), meanDataHead)
