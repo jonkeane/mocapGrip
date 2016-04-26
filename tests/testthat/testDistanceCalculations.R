00848e2e9d888cf75dce4d3834812d4362891efe
Jonathan Keane <jkeane@gmail.com>
2016-04-26 15:52:16 -0500

    added file.path to tests

--- tests/testthat/testDistanceCalculations.R
+++ tests/testthat/testDistanceCalculations.R
@@ -1,14 +1,14 @@
 library(mocapGrip)
 context("distance calculationss")
 
-load("./extractedMarkerData.Rdata") # markerDataHead
-load("./dist57.RData") # dist57head
+load(file.path("extractedMarkerData.Rdata")) # markerDataHead
+load(file.path("dist57.RData")) # dist57head
 
 test_that("calculateDistances returns the correct distances", {
   expect_equal(mocapGrip:::calculateDistances(markerDataHead, c(5,7)), dist57head)
 })
 
-load("./meanData.Rdata") # meanDataHead
+load(file.path("meanData.Rdata")) # meanDataHead
 
 test_that("meanOnAxis returns the correct distances", {
   expect_equal(mocapGrip:::meanOnAxis(markerDataHead, c(0, 1, 2, 3, 4), axis ="Y"), meanDataHead)
