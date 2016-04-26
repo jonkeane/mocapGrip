13a6c3fdc9c5c43da643f868350577bfc5568d1c
Jonathan Keane <jkeane@gmail.com>
2016-04-26 14:33:45 -0500

    Added tests for checking if the directory structure testing is appropriate.

--- tests/testthat/testmakeOneElanFile.R
+++ tests/testthat/testmakeOneElanFile.R
@@ -5,3 +5,7 @@ test_that("makeOneElanFile returns warnings for nonexistent file", {
   expect_warning(mocapGrip:::makeOneElanFile("There/is/simply/no/way/this/file/exists"))
   expect_equal(mocapGrip:::makeOneElanFile("There/is/simply/no/way/this/file/exists"), 1)
 })
+
+test_that("makeOneElanFile returns an error if the path to the video file is misformed", {
+  expect_error(mocapGrip:::makeOneElanFile("/etc/"))
+})
