09f19471233c0d0b888d6109cf629523be13fcd5
Jonathan Keane <jkeane@gmail.com>
2016-04-26 15:08:51 -0500

    Changed test data location for travis

--- tests/testthat/testDistanceCalculations.R
+++ tests/testthat/testDistanceCalculations.R
@@ -1,14 +1,14 @@
 library(mocapGrip)
 context("distance calculationss")
 
-load("extractedMarkerData.Rdata") # markerDataHead
-load("dist57.RData") # dist57head
+load("./extractedMarkerData.Rdata") # markerDataHead
+load("./dist57.RData") # dist57head
 
 test_that("calculateDistances returns the correct distances", {
   expect_equal(mocapGrip:::calculateDistances(markerDataHead, c(5,7)), dist57head)
 })
 
-load("meanData.Rdata") # meanDataHead
+load("./meanData.Rdata") # meanDataHead
 
 test_that("meanOnAxis returns the correct distances", {
   expect_equal(mocapGrip:::meanOnAxis(markerDataHead, c(0, 1, 2, 3, 4), axis ="Y"), meanDataHead)
