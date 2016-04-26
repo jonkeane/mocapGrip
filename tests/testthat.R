a3a916888d09137a2509d6f475538a76c9455eab
Jonathan Keane <jkeane@gmail.com>
2016-04-26 17:33:45 -0500

    Trying a new location for loading test data frames

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,8 @@
 library(testthat)
 library(mocapGrip)
 
+load(file.path('extractedMarkerData.Rdata')) # markerDataHead
+load(file.path('dist57.RData')) # dist57head
+load(file.path('meanData.Rdata')) # meanDataHead
+
 test_check("mocapGrip")
--- tests/testthat/testDistanceCalculations.R
+++ tests/testthat/testDistanceCalculations.R
@@ -2,13 +2,9 @@ library(mocapGrip)
 context("distance calculationss")
 
 test_that("calculateDistances returns the correct distances", {
-  load(file.path('extractedMarkerData.Rdata')) # markerDataHead
-  load(file.path('dist57.RData')) # dist57head
   expect_equal(mocapGrip:::calculateDistances(markerDataHead, c(5,7)), dist57head)
 })
 
 test_that("meanOnAxis returns the correct distances", {
-  load(file.path('extractedMarkerData.Rdata')) # markerDataHead
-  load(file.path('meanData.Rdata')) # meanDataHead
   expect_equal(mocapGrip:::meanOnAxis(markerDataHead, c(0, 1, 2, 3, 4), axis ="Y"), meanDataHead)
 })
