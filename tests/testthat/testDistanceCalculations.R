02da45c6e5a30522212a2c86a29fad13b8d17e81
Jonathan Keane <jkeane@gmail.com>
2016-04-26 14:11:53 -0500

    changed where data files are loaded from.

--- tests/testthat/testDistanceCalculations.R
+++ tests/testthat/testDistanceCalculations.R
@@ -1,14 +1,14 @@
 library(mocapGrip)
 context("distance calculationss")
 
-load(system.file("tests","testthat","extractedMarkerData.Rdata", package="mocapGrip")) # markerDataHead
-load(system.file("tests","testthat","dist57.RData", package="mocapGrip")) # dist57head
+load("extractedMarkerData.Rdata") # markerDataHead
+load("dist57.RData") # dist57head
 
 test_that("calculateDistances returns the correct distances", {
   expect_equal(mocapGrip:::calculateDistances(markerDataHead, c(5,7)), dist57head)
 })
 
-load(system.file("tests","testthat","meanData.Rdata", package="mocapGrip")) # meanDataHead
+load("meanData.Rdata") # meanDataHead
 
 test_that("meanOnAxis returns the correct distances", {
   expect_equal(mocapGrip:::meanOnAxis(markerDataHead, c(0, 1, 2, 3, 4), axis ="Y"), meanDataHead)
