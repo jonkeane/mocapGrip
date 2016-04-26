652412cb29c11e2b0dd6d44a52b4a8176f683392
Jonathan Keane <jkeane@gmail.com>
2016-04-26 17:20:08 -0500

    moved meandata

--- tests/testthat/testDistanceCalculations.R
+++ tests/testthat/testDistanceCalculations.R
@@ -7,8 +7,8 @@ test_that("calculateDistances returns the correct distances", {
   expect_equal(mocapGrip:::calculateDistances(markerDataHead, c(5,7)), dist57head)
 })
 
-load(file.path('meanData.Rdata')) # meanDataHead
-
 test_that("meanOnAxis returns the correct distances", {
+  load(file.path('extractedMarkerData.Rdata')) # markerDataHead
+  load(file.path('meanData.Rdata')) # meanDataHead
   expect_equal(mocapGrip:::meanOnAxis(markerDataHead, c(0, 1, 2, 3, 4), axis ="Y"), meanDataHead)
 })
