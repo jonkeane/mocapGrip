3733b2c7f982ee6fa85d56dfaf920018dc6d9fe1
Jonathan Keane <jkeane@gmail.com>
2016-04-27 14:50:14 -0500

    Removed data loading for test to satisfy travis.

--- tests/testthat/testDistanceCalculations.R
+++ tests/testthat/testDistanceCalculations.R
@@ -1,13 +1,12 @@
 library(mocapGrip)
 context("distance calculationss")
 
-
-print(getwd())
-print(list.files())
-
-load(file.path('extractedMarkerData.Rdata')) # markerDataHead
-load(file.path('dist57.RData')) # dist57head
-load(file.path('meanData.Rdata')) # meanDataHead
+# Doesn't work on travis see http://stackoverflow.com/questions/36877168/using-load-to-load-data-from-an-rdata-file-during-a-testthat-test-on-travis
+# For now these have been moved to the /data directory.
+# This also means the data will always be exported if needed, which is not great, but not the worst.
+# load(file.path('extractedMarkerData.Rdata')) # markerDataHead
+# load(file.path('dist57.RData')) # dist57head
+# load(file.path('meanData.Rdata')) # meanDataHead
 
 test_that("calculateDistances returns the correct distances", {
   expect_equal(mocapGrip:::calculateDistances(markerDataHead, c(5,7)), dist57head)
