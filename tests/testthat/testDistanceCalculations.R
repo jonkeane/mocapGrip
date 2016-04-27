342cf3bf982a5ff4a2e564260eb2fe2696bef833
Jonathan Keane <jkeane@gmail.com>
2016-04-27 15:22:56 -0500

    Aligned capitalization to match expectations (.*.Rdata)

--- tests/testthat/testDistanceCalculations.R
+++ tests/testthat/testDistanceCalculations.R
@@ -4,9 +4,9 @@ context("distance calculationss")
 # Doesn't work on travis see http://stackoverflow.com/questions/36877168/using-load-to-load-data-from-an-rdata-file-during-a-testthat-test-on-travis
 # For now these have been moved to the /data directory.
 # This also means the data will always be exported if needed, which is not great, but not the worst.
-# load(file.path('extractedMarkerData.Rdata')) # markerDataHead
-# load(file.path('dist57.RData')) # dist57head
-# load(file.path('meanData.Rdata')) # meanDataHead
+load(file.path('extractedMarkerData.RData')) # markerDataHead
+load(file.path('dist57.RData')) # dist57head
+load(file.path('meanData.RData')) # meanDataHead
 
 test_that("calculateDistances returns the correct distances", {
   expect_equal(mocapGrip:::calculateDistances(markerDataHead, c(5,7)), dist57head)
