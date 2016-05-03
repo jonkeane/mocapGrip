421ec9d0a07625c1ff9721bf956fb65e970364de
Jonathan Keane <jkeane@gmail.com>
2016-05-03 15:14:41 -0500

    Fixed warning from popping up in test.

--- tests/testthat/testmakeOneElanFile.R
+++ tests/testthat/testmakeOneElanFile.R
@@ -2,8 +2,8 @@ library(mocapGrip)
 context("makeOneElanFile")
 
 test_that("makeOneElanFile returns warnings for nonexistent file", {
-  expect_warning(mocapGrip:::makeOneElanFile("There/is/simply/no/way/this/file/exists"))
-  expect_equal(mocapGrip:::makeOneElanFile("There/is/simply/no/way/this/file/exists"), 1)
+  expect_warning(testOut <- mocapGrip:::makeOneElanFile("There/is/simply/no/way/this/file/exists"))
+  expect_equal(testOut, 1)
 })
 
 test_that("makeOneElanFile returns an error if the path to the video file is misformed", {
