b946ca62e29a0d63dd9ebb60d2082a1383466716
Jonathan Keane <jkeane@gmail.com>
2016-05-04 14:53:13 -0500

    Fixed a typo in the documentation for extractAnnotations()

--- R/extractAnnotations.R
+++ R/extractAnnotations.R
@@ -1,7 +1,7 @@
 
 #' Extract (and check) annotations from elan files.
 #'
-#' This function takes in a vector of elan files (*.eaf) and extracts the annotations from them. Additionally it preforms some checking to makes ure that the annotations are in the format we expect. This won't catch all possible annotation errors, but should catch many of the most common ones.
+#' This function takes in a vector of elan files (*.eaf) and extracts the annotations from them. Additionally it preforms some checking to make sure that the annotations are in the format we expect. This won't catch all possible annotation errors, but should catch many of the most common ones.
 #'
 #' @param files A vector of elan files
 #' @param destDir The destination directory where the extract csvs should be saved
