5a8e7497f4db870a8392371f35c77a1448e154eb
Jonathan Keane <jkeane@gmail.com>
2016-05-06 16:19:01 -0500

    Tests for analyze.R

--- tests/testthat/testAnalyze.R
+++ tests/testthat/testAnalyze.R
@@ -8,21 +8,42 @@ test_that("equation (formula) generation works", {
                  "maxGrip~stickcmScaled+fins+(1+stickcmScaled+fins|obsisSubj)"))
 })
 
+modelsAction <- mocapGrip:::fitLMER(mocapGrip:::eqsGen2preds(outcome="maxGrip", predictor1="stickcmScaled", predictor2="fins"), data=pureReplication$action$data)
 test_that("fitting lmer function returns the right shape, and handles warnings", {
-  expect_warning(models <- mocapGrip:::fitLMER(mocapGrip:::eqsGen2preds(outcome="maxGrip", predictor1="stickcmScaled", predictor2="fins"), data=pureReplication$action$data))
   # these might change if optimizers change in lme
-  expect_equal(models$`maxGrip~stickcmScaled*fins+(1+stickcmScaled*fins|obsisSubj)`$converged, TRUE)
-  expect_equal(models$`maxGrip~stickcmScaled*fins+(1+stickcmScaled+fins|obsisSubj)`$converged, FALSE)
-  expect_equal(models$`maxGrip~stickcmScaled+fins+(1+stickcmScaled+fins|obsisSubj)`$converged, TRUE)
+  expect_equal(modelsAction$`maxGrip~stickcmScaled*fins+(1+stickcmScaled*fins|obsisSubj)`$converged, TRUE)
+  expect_equal(modelsAction$`maxGrip~stickcmScaled*fins+(1+stickcmScaled+fins|obsisSubj)`$converged, FALSE)
+  expect_equal(modelsAction$`maxGrip~stickcmScaled+fins+(1+stickcmScaled+fins|obsisSubj)`$converged, TRUE)
 
 
-  expect_silent(models <- mocapGrip:::fitLMER(mocapGrip:::eqsGen2preds(outcome="meanGrip", predictor1="stickcmScaled", predictor2="fins"), data=pureReplication$estimation$data))
+  expect_silent(modelsEst <- mocapGrip:::fitLMER(mocapGrip:::eqsGen2preds(outcome="meanGrip", predictor1="stickcmScaled", predictor2="fins"), data=pureReplication$estimation$data))
   # these might change if optimizers change in lme
-  expect_equal(models$`meanGrip~stickcmScaled*fins+(1+stickcmScaled*fins|obsisSubj)`$converged, TRUE)
-  expect_equal(models$`meanGrip~stickcmScaled*fins+(1+stickcmScaled+fins|obsisSubj)`$converged, TRUE)
-  expect_equal(models$`meanGrip~stickcmScaled+fins+(1+stickcmScaled+fins|obsisSubj)`$converged, TRUE)
+  expect_equal(modelsEst$`meanGrip~stickcmScaled*fins+(1+stickcmScaled*fins|obsisSubj)`$converged, TRUE)
+  expect_equal(modelsEst$`meanGrip~stickcmScaled*fins+(1+stickcmScaled+fins|obsisSubj)`$converged, TRUE)
+  expect_equal(modelsEst$`meanGrip~stickcmScaled+fins+(1+stickcmScaled+fins|obsisSubj)`$converged, TRUE)
 })
 
 test_that("fitting lmer function returns the right shape, and handles warnings", {
-  expect_error(mocapGrip:::fitTheRightModels(type="this is not a type", data=list()))
+  expect_error(mocapGrip:::fitModels(type="this is not a type", data=list()))
+})
+
+test_that("the model chosing function works forwards", {
+  expect_silent(modsChoseAction <- findTheBestModel(modelsAction))
+  expect_equal(modsChoseAction$allModels, modelsAction)
+  expect_equal(names(modsChoseAction$bestModel), "maxGrip~stickcmScaled*fins+(1+stickcmScaled*fins|obsisSubj)")
+  expect_equal(names(modsChoseAction), c("bestModel", "allModels"))
+  })
+
+test_that("the model chosing function works backwords", {
+  expect_silent(modsChoseAction <- findTheBestModel(modelsAction, last = TRUE))
+  expect_equal(modsChoseAction$allModels, modelsAction)
+  expect_equal(names(modsChoseAction), c("bestModel", "allModels"))
+  expect_equal(names(modsChoseAction$bestModel), "maxGrip~stickcmScaled+fins+(1+stickcmScaled+fins|obsisSubj)")
+})
+
+test_that("modelAllData runs through data", {
+  expect_warning(dataModeled <- modelAllData(pureReplication))
+  expect_equal(names(dataModeled$action), c("data", "warnings", "models"))
+  expect_equal(dataModeled$action$data, pureReplication$action$data)
+  expect_equal(dataModeled$action$warnings, pureReplication$action$warnings)
 })
