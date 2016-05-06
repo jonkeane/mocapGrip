89013d745fe7abed72ff83e17be120fc23a066f0
Jonathan Keane <jkeane@gmail.com>
2016-05-06 11:29:13 -0500

    Added reportWriter.R and skeleton-outlined the logic for report writting.

---
+++ R/reportWriter.R
@@ -0,0 +1,18 @@
+# add kinds of models?
+# export(!)
+makeReport <- function(data){
+  # parse data?
+
+  # fit models from analyze
+  models <- fitLMER(eqsGen2preds(outcome="maxGrip", predictor1="stickcmScaled", predictor2="fins"), data=pureReplication$action$data)
+
+  # pick the best model (add warnings here?)
+
+  # read/write template
+
+  # render
+  # change path
+  rmarkdown::render("./inst/reportTemplateReplication.Rmd", params = list(data=pureReplication))
+
+}
+
