f461c89e086e2222edd99f46e47f9988094f7773
Jonathan Keane <jkeane@gmail.com>
2016-05-08 21:58:01 -0500

    Collapsed analyze and report writer tests to limit the number of times models must be fit.

--- tests/testthat/testAnalyzeAndReportWriter.R
+++ tests/testthat/testAnalyzeAndReportWriter.R
@@ -49,9 +49,32 @@ test_that("the model chosing function works backwords", {
   expect_equal(names(modsChoseAction$bestModel), "maxGrip~stickcmScaled+fins+(1+stickcmScaled+fins|obsisSubj)")
 })
 
+dataModeled <- modelAllData(pureReplication)
+
 test_that("modelAllData runs through data", {
-  expect_warning(dataModeled <- modelAllData(pureReplication))
   expect_equal(names(dataModeled$action), c("data", "warnings", "models"))
   expect_equal(dataModeled$action$data, pureReplication$action$data)
   expect_equal(dataModeled$action$warnings, pureReplication$action$warnings)
 })
+
+context("report writer")
+
+test_that("replaceText works with a selection of texts", {
+  expect_equal(mocapGrip:::replaceText(list("##", "$title"), modelStructure$models$analyses$action$narrative),c("##", "Maximum Grip aperture (on reach to grasp)"))
+  expect_equal(mocapGrip:::replaceText(list("##", "$title", "$intro"), modelStructure$models$analyses$action$narrative),c("##", "Maximum Grip aperture (on reach to grasp)", "The maximum grip aperture is the maximum distance between the markers on the thumb and index finger during the period between when the hand left the table and when it touched the stick (this period is labeled as *grip* in our annotation system)."))
+})
+
+# test_that("reaplceText works with lists in the replacements", {
+#   expect_equal(mocapGrip:::replaceText(list("$predictorVariables"), modelStructure$action),c("\n* the size of the stick in centimeters (centered at 8 cm, where 1 unit difference is 1 cm difference in stick size)\n* the configuration of the fins (closed, none, open, where closed is the reference level)\n* the interaction between the size of the stick and configuration of fins."))
+# })
+
+test_that("", {
+  expect_equal(mocapGrip:::formatGatherReplacements("action", dataModeled), list("title" = "Maximum Grip aperture (on reach to grasp)",
+                                                                                "intro" = "The maximum grip aperture is the maximum distance between the markers on the thumb and index finger during the period between when the hand left the table and when it touched the stick (this period is labeled as *grip* in our annotation system).",
+                                                                                "outcome" = "maximum grip aperture (in mm)",
+                                                                                "predictorVariables" = "\n* the size of the stick in centimeters (centered at 8 cm, where 1 unit difference is 1 cm difference in stick size)\n* the configuration of the fins (closed, none, open, where closed is the reference level)\n* the interaction between the size of the stick and configuration of fins",
+                                                                                "includeInteractionInGroup" = "(including interactions)",
+                                                                                "groupingVariable" = "by subject"))
+
+})
+
