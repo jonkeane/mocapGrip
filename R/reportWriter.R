d3a00a188eab4901df31b7b92d135326bddfec84
Jonathan Keane <jkeane@gmail.com>
2016-05-08 21:59:01 -0500

    Continuing work on report Writer

--- R/reportWriter.R
+++ R/reportWriter.R
@@ -4,7 +4,7 @@ makeReport <- function(data){
   # parse data?
 
   # fit models from analyze
-  models <- fitLMER(eqsGen2preds(outcome="maxGrip", predictor1="stickcmScaled", predictor2="fins"), data=pureReplication$action$data)
+  # models <- fitLMER(eqsGen2preds(outcome="maxGrip", predictor1="stickcmScaled", predictor2="fins"), data=pureReplication$action$data)
 
   # pick the best model (add warnings here?)
 
@@ -12,7 +12,6 @@ makeReport <- function(data){
 
   # render
   # change path
-  rmarkdown::render("./inst/reportTemplateReplication.Rmd", params = list(data=pureReplication))
 
 }
 
@@ -30,9 +29,60 @@ replaceText <- Vectorize(function(text, reps){
     return(text)
   }
   replText <- reps[[substring(text, 2)]]
-  if(length(replText)>1){
-    return(paste0("\n* ", replText, collapse = ""))
-  }
+
   return(replText)
 }, vectorize.args = "text", USE.NAMES = FALSE)
+
+
+writeMarkdown <- function(modeledData,
+                          markdownPath = "./report.Rmd",
+                          modelStructure = system.file("modelStructure.json", package = "mocapGrip", mustWork=TRUE)
+                          ){
+  modelStructure
+  analysisSkel <- readLines("./inst/analysisSkeleton.Rmd")
+  header <- readLines("./inst/header.Rmd")
+
+  tmpFile <- "./test.Rmd"
+  con <- file(tmpFile)
+  on.exit(close(con))
+  writeLines(c(header, cleanText(analysisSkel, modelStructure$action)), con)
+}
+
+formatGatherReplacements = function(analysis, modeledData) {
+  # grab variables from the analyses structure
+  outcome <- modelStructure$models$analyses[[analysis]]$variablesToUse$outcome
+  predictor1 <- modelStructure$models$analyses[[analysis]]$variablesToUse$predictor1
+  predictor2 <- modelStructure$models$analyses[[analysis]]$variablesToUse$predictor2
+  grouping1 <- modelStructure$models$analyses[[analysis]]$variablesToUse$grouping1
+
+  # check if there is an interaction in the predictors.
+  if(names(modeledData[[analysis]]$models$bestModel) %in% c("interactionInPredAndGroup", "interactionInPred")){
+    interaction <- modelStructure$variableExplanations[[paste(predictor1, predictor2, sep = "X")]]
+  } else {
+    interaction <- NULL
+  }
+
+  # make and format $predictorVariables
+  predictorVariables <- paste0("\n* ",
+                               c(modelStructure$variableExplanations[[predictor1]],
+                                 modelStructure$variableExplanations[[predictor2]],
+                                 interaction),
+                               collapse = "")
+
+
+  # add interaction text if  the selected model is $includeInteractionInGroup
+  if(names(modeledData[[analysis]]$models$bestModel)=="interactionInPredAndGroup"){
+    includeInteractionInGroup <- "(including interactions)"
+  }
+
+  c(modelStructure$models$analyses[[analysis]]$narrative, # gather narratives
+    "outcome" = modelStructure$variableExplanations[[outcome]],
+    "predictorVariables" = predictorVariables,
+    "includeInteractionInGroup" = includeInteractionInGroup,
+    "groupingVariable" = modelStructure$variableExplanations[[grouping1]]) # outcomeVariable
+}
+
+# rmarkdown::render(tmpFile, params = list(data=dataModelled$action))
+
+
 
