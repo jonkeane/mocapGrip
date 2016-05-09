99f13ad90dad1bc7c81ec5da7ca338cae4ea7c86
Jonathan Keane <jkeane@gmail.com>
2016-05-09 09:24:07 -0500

    Added tests for models without interactions.

--- tests/testthat/testAnalyzeAndReportWriter.R
+++ tests/testthat/testAnalyzeAndReportWriter.R
@@ -49,13 +49,24 @@ test_that("the model chosing function works backwords", {
   expect_equal(names(modsChoseAction$bestModel), "maxGrip~stickcmScaled+fins+(1+stickcmScaled+fins|obsisSubj)")
 })
 
+
+### Setup dataModeled for test
 dataModeled <- modelAllData(pureReplication)
+
+# this takes the last model to fit as the best.
+# In the current setup of modelStructure.json, this is the simplest model.
+dataModeledSimplest <- modelAllData(pureReplication, last = TRUE)
+
 
 test_that("modelAllData runs through data", {
   expect_equal(names(dataModeled$action), c("data", "warnings", "models"))
   expect_equal(dataModeled$action$data, pureReplication$action$data)
   expect_equal(dataModeled$action$warnings, pureReplication$action$warnings)
-})
+
+  expect_equal(names(dataModeledSimplest$action), c("data", "warnings", "models"))
+  expect_equal(dataModeledSimplest$action$data, pureReplication$action$data)
+  expect_equal(dataModeledSimplest$action$warnings, pureReplication$action$warnings)
+  })
 
 context("report writer")
 
@@ -65,12 +76,21 @@ test_that("replaceText works with a selection of texts", {
 })
 
 test_that("replaceText works with formatGatherReplacements", {
+  ## check dataModeled
   expect_equal(mocapGrip:::replaceText(list("$title"), formatGatherReplacements("action", dataModeled)),c("Maximum Grip aperture (on reach to grasp)"))
   expect_equal(mocapGrip:::replaceText(list("$intro"), formatGatherReplacements("action", dataModeled)),c("The maximum grip aperture is the maximum distance between the markers on the thumb and index finger during the period between when the hand left the table and when it touched the stick (this period is labeled as *grip* in our annotation system)."))
   expect_equal(mocapGrip:::replaceText(list("$outcomeVariable"), formatGatherReplacements("action", dataModeled)),c("maximum grip aperture (in mm)"))
   expect_equal(mocapGrip:::replaceText(list("$predictorVariables"), formatGatherReplacements("action", dataModeled)),c("\n* the size of the stick in centimeters (centered at 8 cm, where 1 unit difference is 1 cm difference in stick size)\n* the configuration of the fins (closed, none, open, where closed is the reference level)\n* the interaction between the size of the stick and configuration of fins"))
   expect_equal(mocapGrip:::replaceText(list("$includeInteractionInGroup"), formatGatherReplacements("action", dataModeled)),c("(including interactions)"))
   expect_equal(mocapGrip:::replaceText(list("$groupingVariable"), formatGatherReplacements("action", dataModeled)),c("by subject"))
+
+  ## check dataModeledSimplest
+  expect_equal(mocapGrip:::replaceText(list("$title"), formatGatherReplacements("action", dataModeledSimplest)),c("Maximum Grip aperture (on reach to grasp)"))
+  expect_equal(mocapGrip:::replaceText(list("$intro"), formatGatherReplacements("action", dataModeledSimplest)),c("The maximum grip aperture is the maximum distance between the markers on the thumb and index finger during the period between when the hand left the table and when it touched the stick (this period is labeled as *grip* in our annotation system)."))
+  expect_equal(mocapGrip:::replaceText(list("$outcomeVariable"), formatGatherReplacements("action", dataModeledSimplest)),c("maximum grip aperture (in mm)"))
+  expect_equal(mocapGrip:::replaceText(list("$predictorVariables"), formatGatherReplacements("action", dataModeledSimplest)),c("\n* the size of the stick in centimeters (centered at 8 cm, where 1 unit difference is 1 cm difference in stick size)\n* the configuration of the fins (closed, none, open, where closed is the reference level)"))
+  expect_equal(mocapGrip:::replaceText(list("$includeInteractionInGroup"), formatGatherReplacements("action", dataModeledSimplest)),list(NULL))
+  expect_equal(mocapGrip:::replaceText(list("$groupingVariable"), formatGatherReplacements("action", dataModeledSimplest)),c("by subject"))
 })
 
 test_that("formatGatherReplacements works", {
