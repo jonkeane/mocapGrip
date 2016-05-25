b76a5aea4a777b30f9e92407998d43dc1e6ce2c1
Jonathan Keane <jkeane@gmail.com>
2016-05-25 12:08:45 -0500

    Added tests for succesful data extraction

--- tests/testthat/testExtractMocapDataFromAnnotations.R
+++ tests/testthat/testExtractMocapDataFromAnnotations.R
@@ -3,5 +3,10 @@ context("extract MocapData from annotations")
 
 test_that("extractMocapDataFromAnnotations errors appropriately", {
   expect_error(extractMocapDataFromAnnotations("./", "./"))
-  expect_error(extractMocapDataFromAnnotations(".toTest.Rmd", "./"))
+  expect_error(extractMocapDataFromAnnotations("./toTest.Rmd", "./"))
+})
+
+test_that("extractMocapDataFromAnnotations runs fine on minimal data", {
+  expect_message(extractMocapDataFromAnnotations("../../inst/extData/minimalSubset/elanFilesCompleted/GRI_070/GRI_070-SESSION_001-TRIAL_00?.eaf",
+                                                destDir = "../../inst/extData/minimalSubset/extractedData/"))
 })
