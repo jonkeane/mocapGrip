9c29a6bcd32ee2e52aeaec6dafe791a1c8ba9aa0
Jonathan Keane <jkeane@gmail.com>
2016-05-25 14:25:00 -0500

    Added hidden files to keep folders from being deleted in build. Ignoring mocapdata means that those tests can't be run during build.

--- tests/testthat/testExtractMocapDataFromAnnotations.R
+++ tests/testthat/testExtractMocapDataFromAnnotations.R
@@ -10,21 +10,20 @@ test_that("path fixing works", {
   expect_output(fixPaths("./GRIP/elanFilesCompleted/GRI_070/GRI_070-SESSION_001-TRIAL_00?.eaf"))
 })
 
-# these tests fail with check, but not with test.
-# test_that("extractMocapDataFromAnnotations runs fine on minimal data", {
-#   expect_message(extractMocapDataFromAnnotations("./GRIP/elanFilesCompleted/GRI_070/GRI_070-SESSION_001-TRIAL_00?.eaf",
-#                                                 destDir = "./extractedData/"))
-#   expect_equal(read.csv("./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_002.csv"),
-#                read.csv("./extractedDataGoldStd/GRI_070/GRI_070-SESSION_001-TRIAL_002.csv"))
-#   expect_equal(read.csv("./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_005.csv"),
-#                read.csv("./extractedDataGoldStd/GRI_070/GRI_070-SESSION_001-TRIAL_005.csv"))
-#   expect_equal(read.csv("./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_009.csv"),
-#                read.csv("./extractedDataGoldStd/GRI_070/GRI_070-SESSION_001-TRIAL_009.csv"))
-#   # clean up
-#   unlink(c("./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_002.csv",
-#            "./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_005.csv",
-#            "./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_009.csv"))
-# })
+test_that("extractMocapDataFromAnnotations runs fine on minimal data", {
+  expect_message(extractMocapDataFromAnnotations("./GRIP/elanFilesCompleted/GRI_070/GRI_070-SESSION_001-TRIAL_00?.eaf",
+                                                destDir = "./extractedData/"))
+  expect_equal(read.csv("./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_002.csv"),
+               read.csv("./extractedDataGoldStd/GRI_070/GRI_070-SESSION_001-TRIAL_002.csv"))
+  expect_equal(read.csv("./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_005.csv"),
+               read.csv("./extractedDataGoldStd/GRI_070/GRI_070-SESSION_001-TRIAL_005.csv"))
+  expect_equal(read.csv("./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_009.csv"),
+               read.csv("./extractedDataGoldStd/GRI_070/GRI_070-SESSION_001-TRIAL_009.csv"))
+  # clean up
+  unlink(c("./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_002.csv",
+           "./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_005.csv",
+           "./extractedData/GRI_070/GRI_070-SESSION_001-TRIAL_009.csv"))
+})
 
 
 
